9e981c35e88f32cd2b930b9325ec39cc609554be
Lance Desk <lncedesk@gmail.com>
2023-12-18 12:31:05 -0800

    Double linked list

--- 0x00-challenge/delete_dnodeint_at_index.c
+++ 0x00-challenge/delete_dnodeint_at_index.c
@@ -21,17 +21,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	saved_head = *head;
 	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
+
+	/* Handle deletion at the beginning (index 0) */
+	if (index == 0)
 	{
 		tmp = (*head)->next;
 		free(*head);
@@ -40,14 +32,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			tmp->prev = NULL;
 		}
+		return (1);
 	}
-	else
+
+	while (p < index && *head != NULL)
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
+		*head = (*head)->next;
+		p++;
 	}
+	if (p != index || *head == NULL)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+
+	(*head)->prev->next = (*head)->next;
+	if ((*head)->next != NULL)
+		(*head)->next->prev = (*head)->prev;
+
+	free(*head);
+	*head = saved_head;
+
 	return (1);
 }
