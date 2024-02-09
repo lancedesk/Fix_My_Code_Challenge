b41694cff8595645b23ab1d4c490571e9f13d35a
Lance Desk <lncedesk@gmail.com>
2024-02-09 04:03:21 -0800

    Square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,31 +7,56 @@ contains class square
 
 class Square():
     '''
-    Class square
+    Class representing a square.
+
+    Attributes:
+    - width (int): The width of the square.
+    - height (int): The height of the square.
     '''
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
-        '''Class Init method'''
+        '''
+        Initializes a square object with optional width and height.
+
+        Parameters:
+        - args: Positional arguments.
+        - kwargs: Keyword arguments to set attributes.
+        '''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Calculate the area of the square.
+
+        Returns:
+        - int: The area of the square.
+        """
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        '''Perimeter of the square'''
+        '''
+        Calculate the perimeter of the square.
+
+        Returns:
+        - int: The perimeter of the square.
+        '''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        '''Class handle print'''
+        '''
+        Return a string representation of the square.
+
+        Returns:
+        - str: A string representation of the square.
+        '''
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    '''Test Class'''
+    '''
+    Test Class
+    '''
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
